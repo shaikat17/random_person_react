c896a34cfdd6912b74d4392274c3bb3374f3a1d9
shaikat17 <shaikatpal56@gmail.com>
2023-01-29 00:10:48 +0600

    woking on api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa'
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
+  // State Variables
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [value, setValue] = useState("random person");
+  const [title, setTitle] = useState("name");
+
+  const getPerson = async () => {
+
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+  const handleValue = (e) => {
+    console.log(e.target);
+    // if (e.target.classList.contains('icon')) {
+    //   const newValue = e.target.dataset.label
+    //   setTitle(newValue)
+    //   setValue(person[newValue])
+    // }
+  }
+
   return (
     <main>
-      <h1>Random Person</h1>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.img) || defaultImage}
+            alt="image-block"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type="button" onClick={getPerson}>
+            {loading ? "loading..." : "random user"}
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
